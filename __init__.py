fbf77cb592605b100959dfb2fa50cd4c22601ff4
Maxim Petrov <mmrmaximuzz@gmail.com>
2024-12-07 11:18:16 +0300

    Initial commit

---
+++ __init__.py
@@ -0,0 +1,6 @@
+"""Simulated scheduling.
+
+Tool which helps to simulate parallel/concurrent execution. Could be useful in
+developing or debugging concurrent algorithms before actually write any code
+for a target platform.
+"""
