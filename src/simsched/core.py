b157e0031583b3204792f4cc414765b783798e4e
Maxim Petrov <mmrmaximuzz@gmail.com>
2024-12-07 14:39:41 +0300

    First dirty prototype

---
+++ src/simsched/core.py
@@ -0,0 +1,75 @@
+"""Core components for simsched engine."""
+
+import contextlib
+import random
+from collections.abc import Generator
+from typing import TypeAlias
+
+
+TRACE = []
+
+SimThread: TypeAlias = Generator[str, str, None]
+
+
+def schedule() -> SimThread:
+    cmd = yield "seqpoint"
+    while True:
+        match cmd:
+            case "poll":
+                cmd = yield "ready"  # always ready to continue
+                continue
+            case "continue":
+                break
+            case _:
+                raise AssertionError(f"unknown schedule command {cmd}")
+
+
+def thrd1() -> SimThread:
+    yield from schedule()
+    TRACE.append("T1 1")
+    yield from schedule()
+    TRACE.append("T1 2")
+
+
+def thrd2() -> SimThread:
+    yield from schedule()
+    TRACE.append("T2 1")
+    yield from schedule()
+    TRACE.append("T2 2")
+
+
+def poll(threads: list[SimThread]) -> list[SimThread]:
+    runnables = []
+
+    for t in threads:
+        with contextlib.suppress(StopIteration):
+            match t.send("poll"):
+                case "ready":
+                    runnables.append(t)
+
+    return runnables
+
+
+def test_core():
+    t1 = thrd1()
+    t2 = thrd2()
+    assert isinstance(t1, Generator)
+    assert isinstance(t2, Generator)
+
+    threads = [t1, t2]
+
+    # spawn all the generators
+    for t in threads:
+        next(t)
+
+    while True:
+        runnables = poll(threads)
+        if not runnables:
+            print("done - no runnable threads available")
+            break
+
+        t = random.choice(runnables)
+        with contextlib.suppress(StopIteration):
+            t.send("continue")
+
+    print(TRACE)
